95d5834c71916040cc0cf5a39e07a97ac4526775
chiragsoni152 <chirag.soni152@gmail.com>
2017-08-13 12:07:22 +0530

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -1,12 +1,12 @@
 library(dplyr)
 library(lubridate)
 
-# reading relevant data
-data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+# read the data
+data_graph <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
 
-# clearing name row and unrelevant variables, creating numerics
-data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+# clearing unrelevant variables
+data_graph <- data_graph %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
 
-# create histogram
+# below creates the histogram
 hist(data$V3, col = "red", main = "Global Active Power",
      xlab = "Global Active Power (killowatts)", ylab = "Frequency")
