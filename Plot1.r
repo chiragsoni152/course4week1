7827956467e0201e5bba3e4fbefbb5571f823f66
chiragsoni152 <chirag.soni152@gmail.com>
2017-08-13 12:28:21 +0530

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -1,12 +1,16 @@
-library(dplyr)
-library(lubridate)
+#below code creates the first graph in assignment
+#data is collected from Electric power consumption as per forked library
 
-# read the data
-data_graph <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
 
-# clearing unrelevant variables
-data_graph <- data_graph %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+#first read the relevant data from dates mentioned in assignment
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+graph_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-# below creates the histogram
-hist(data$V3, col = "red", main = "Global Active Power",
-     xlab = "Global Active Power (killowatts)", ylab = "Frequency")
+#below code creates Global Active Power graph
+globalActivePower <- as.numeric(graph_data$Global_active_power)
+#relevant dimensions as mentioned
+
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
